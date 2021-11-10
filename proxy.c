90fdeb4c922bb101ff78737cd7a7bdb8b3960411
contrarythought <59274215+contrarythought@users.noreply.github.com>
2021-11-10 02:28:57 -0500

    don't hardcode the request

--- proxy.c
+++ proxy.c
@@ -178,7 +178,7 @@ void execute_request(int req_type, char *request, int client_fd) {
                 }
                 return;
             }
-            
+               
             /* send the actual GET request to the remote host, and then return the HTML string */
             if(send(sock_fd, "GET / HTTP/1.0\r\n\r\n", strlen("GET / HTTP/1.0\r\n\r\n"), 0) == -1) {
                 printf("Failed to send request to remote host\n");
