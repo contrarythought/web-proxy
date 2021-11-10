ec64b2988e52bec91cea638c30ccce5881b7994b
contrarythought <59274215+contrarythought@users.noreply.github.com>
2021-11-10 16:33:38 -0500

    Update networking.h

--- networking.h
+++ networking.h
@@ -6,6 +6,17 @@
 #include <netdb.h> /* for looking up hosts via DNS (struct hostent, gethostbyname()) */
 
 #define WEB_PORT 80
+#define GET_REQ "GET "
+#define POST_REQ "POST "
+#define PUT_REQ "PUT "
+
+#define HTTP_FRAME "http://www."
+#define WWW_FRAME " www."
+#define DOT_COM ".com"
+#define DOT_ORG ".org"
+#define DOT_EDU ".edu"
+#define DOT_GOV ".gov"
+#define DOT_NET ".net"
 
 enum REQUEST_TYPE {
     GET = 1,
