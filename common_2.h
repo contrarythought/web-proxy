0a780c9ab98b7da5264fdd976a27dec1a4fc48e0
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2021-12-14 16:55:20 -0500

    Create common_2.h

---
+++ common_2.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+
+void usage(char *program_name, char *msg);
+void err(char *msg);
+
+#endif
